304ad24a346cacb612bb40600a0c4d2d764b58a3
Avimita Chatterjee <77707738+avimita-amc8313@users.noreply.github.com>
2026-07-07 18:40:51 -0500

    Add last_updated field to scholar data export

--- scripts/update_scholar.py
+++ scripts/update_scholar.py
@@ -6,10 +6,13 @@ SCHOLAR_ID = "TT9m3j0AAAAJ"
 author = scholarly.search_author_id(SCHOLAR_ID)
 author = scholarly.fill(author, sections=["indices"])
 
+from datetime import datetime, timezone
+
 data = {
     "citations": author.get("citedby", 0),
     "h_index": author.get("hindex", 0),
     "i10_index": author.get("i10index", 0),
+    "last_updated": datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M UTC"),
 }
 
 with open("_data/scholar.yml", "w") as f:
